414f759a99e6e0cc168f81acc27b9a22edc8f7da
FatehKhalsa <manjodhchahal@Manjodhs-MacBook-Pro.local>
2022-10-09 11:57:46 -0700

    Fixed CORS issues

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,10 +2,12 @@ const { authJwt, checkDupsUser } = require("../middlewares");
 const controller = require("../controllers/user.controller");
 module.exports = function(app) {
   app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
+    // res.header(
+    //   "Access-Control-Allow-Headers",
+    //   "x-access-token, Origin, Content-Type, Accept"
+    // );
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
   app.post(
